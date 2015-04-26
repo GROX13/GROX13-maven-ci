f8c6d010196049301e1cbfff82d03aa54c88317c
Giorgi <giorgirokhadze@gmail.com>
2015-04-26 19:05:10 +0400

    Modifying tests

--- test/test/java/giorgi/example/ConcreteObserverOneTest.java
+++ test/test/java/giorgi/example/ConcreteObserverOneTest.java
@@ -1,10 +1,24 @@
 package test.java.giorgi.example;
 
 import static org.junit.Assert.*;
+import main.java.giorgi.example.ConcreteObserverOne;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ConcreteObserverOneTest {
+	ConcreteObserverOne underTest;
+
+	@Before
+	private void setUp() {
+//		underTest = new ConcreteObserverOne(subject, name);
+	}
+
+	@After
+	private void tearDown() {
+		underTest = null;
+	}
 
 	@Test
 	public void testGetSubject() {
--- test/test/java/giorgi/example/ConcreteSubjectOneTest.java
+++ test/test/java/giorgi/example/ConcreteSubjectOneTest.java
@@ -1,10 +1,24 @@
 package test.java.giorgi.example;
 
 import static org.junit.Assert.*;
+import main.java.giorgi.example.ConcreteSubjectOne;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ConcreteSubjectOneTest {
+	ConcreteSubjectOne underTest;
+
+	@Before
+	private void setUp() {
+		underTest = new ConcreteSubjectOne();
+	}
+
+	@After
+	private void tearDown() {
+		underTest = null;
+	}
 
 	@Test
 	public void testGetSubjectState() {
