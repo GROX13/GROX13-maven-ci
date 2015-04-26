606af83b652e57cfe56558fa9a54565cef18faf0
Giorgi <giorgirokhadze@gmail.com>
2015-04-26 21:22:57 +0400

    Updated tests

--- test/test/java/giorgi/example/ConcreteSubjectOneTest.java
+++ test/test/java/giorgi/example/ConcreteSubjectOneTest.java
@@ -1,33 +1,68 @@
 package test.java.giorgi.example;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import main.java.giorgi.example.ConcreteSubjectOne;
+import main.java.giorgi.example.Observer;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ConcreteSubjectOneTest {
+
+	static final String subjectState = "Hello Mock";
 	ConcreteSubjectOne underTest;
 
 	@Before
-	public void setUp() {
-		underTest = new ConcreteSubjectOne();
+	public void setUp() throws Exception {
+		underTest = mock(ConcreteSubjectOne.class);
+		when(underTest.getSubjectState()).thenReturn(subjectState);
 	}
 
 	@After
-	public void tearDown() {
+	public void tearDown() throws Exception {
 		underTest = null;
 	}
 
 	@Test
 	public void testGetSubjectState() {
-		fail("Not yet implemented");
+		String actual = underTest.getSubjectState();
+		assertEquals(subjectState, actual);
+		verify(underTest).getSubjectState();
 	}
 
 	@Test
 	public void testSetSubjectState() {
-		fail("Not yet implemented");
+		underTest.setSubjectState(subjectState);
+		verify(underTest).setSubjectState(subjectState);
+		verify(underTest).notifyObservers();
+	}
+
+	@Test
+	public void testAttach() {
+		underTest.attach(null);
+		verify(underTest).attach(null);
+	}
+
+	@Test
+	public void testDetach() {
+		underTest.detach(null);
+		verify(underTest).detach(null);
+	}
+
+	@Test
+	public void testNotifyObservers() {
+		underTest.notifyObservers();
+		verify(underTest).notifyObservers();
+		// Part two
+		underTest = new ConcreteSubjectOne();
+		Observer observer = mock(Observer.class);
+		underTest.attach(observer);
+		underTest.notifyObservers();
+		verify(observer).update();
 	}
 
 }
