c5c7c5d53ce6b7c7f068acc9f7623ad3b475dc5a
Giorgi <giorgirokhadze@gmail.com>
2015-04-26 18:01:21 +0400

    Created tests
    Created ConcreteObserverOneTest.java and ConcreteSubjectOneTest.java.

---
+++ src/test/java/giorgi/example/ConcreteObserverOneTest.java
@@ -0,0 +1,24 @@
+package test.java.giorgi.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConcreteObserverOneTest {
+
+	@Test
+	public void testGetSubject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSubject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/giorgi/example/ConcreteSubjectOneTest.java
@@ -0,0 +1,19 @@
+package test.java.giorgi.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConcreteSubjectOneTest {
+
+	@Test
+	public void testGetSubjectState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSubjectState() {
+		fail("Not yet implemented");
+	}
+
+}
